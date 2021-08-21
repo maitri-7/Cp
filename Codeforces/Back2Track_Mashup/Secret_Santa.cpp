6624da456cb294b8abd0233f3ad00d71683284f0
maitri-7 <63003714+maitri-7@users.noreply.github.com>
2021-08-22 00:12:05 +0530

    Create Secret_Santa.cpp

---
+++ Codeforces/Back2Track_Mashup/Secret_Santa.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <bitset>
+#include <climits>
+#include <iterator>
+#include <list>
+#include <bits/stdc++.h>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+    // <!-- ordered_set<int>  s;
+    // s.insert(1); 
+    // s.insert(3);
+    // cout << s.order_of_key(2) << endl; // the number of elements in the s less than 2
+    // cout << *s.find_by_order(0) << endl; // print the 0-th smallest number in s(0-based) -->
+//define
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+#define INF (int)1e9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define all(cont) cont.begin(), cont.end()
+#define read(n) ll n;cin>>n;vector<ll> v(n);for(int i=0;i<n;i++){cin>>v[i];}
+#define rep(i,s,n) for(ll i=s;i<n;i++)
+#define repn(i,n,s) for(ll i=n-1;i>=s;i--)
+#define print(arr,n) for(ll i=0;i<n;i++)cout<<arr[i]<<" ";
+#define pi pair<int,int>
+#define pb push_back
+#define read_2d(n,m) ll n,m; cin>>n>>m; vector<vector<ll>> v(n,vector<ll>(m,0));for(ll i=0;i<n;i++){for(ll j=0;j<m;j++){cin>>v[i][j];}}
+#define print_2d(v,n,m) for(ll i=0;i<n;i++){for(int j=0;j<m;j++){cout<<v[i][j]<<" ";}cout<<endl;}
+#define sorta(v) sort(v.begin(),v.end());
+#define sortd(v) sort(v.begin(),v.end(),greater<int>());
+
+
+    
+void solve(){
+    ll n;
+    cin>>n;
+    map<ll,set<ll>> mp;
+    vector<ll> ans(n+1);
+    vector<bool> assigned(n+1);
+    rep(i,0,n){
+        ll a;
+        cin>>a;
+        ans[i+1] = a;
+        assigned[a] = true;
+        mp[a].insert(i+1);
+    }
+    cout<<mp.size()<<endl;
+    vector<ll> left;
+    rep(i,1,n+1)
+        if(!assigned[i]) left.pb(i);
+
+    ll j = 0,a=0;
+    for(auto &it:mp){
+        ll extra = it.second.size();
+        extra -= 1;
+        if(extra > 0){
+            while(extra--){
+                for(auto i:it.second){
+                    if(i != left[j]){
+                        a = i;
+                        ans[i] = left[j]; 
+                        j++;
+                        break;
+                    }
+                }
+                it.second.erase(a);
+            }
+            
+        }
+    }
+    for(ll i=1;i<=n;i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    
+
+
+
+}
+  
+
+int main() {
+    
+    int t;
+    cin>>t;
+      while(t--)
+        solve();
+    
+    return 0;
+}
