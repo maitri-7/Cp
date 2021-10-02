eac2e361d75c5749c498f1cf4a9ad07e8034bec9
maitri-7 <63003714+maitri-7@users.noreply.github.com>
2021-10-02 14:32:24 +0530

    Update Matrix_exponentiation.cpp

--- Maths/Matrix_exponentiation.cpp
+++ Maths/Matrix_exponentiation.cpp
@@ -1,3 +1,24 @@
+vector<vector<ll>> matrixMultiply(vector<vector<ll>>& a,vector<vector<ll>>& b){
+
+    ll R1 = a.size();
+    ll C1 = a[0].size();
+
+    ll R2 = b.size();
+    ll C2 = b[0].size();
+
+    vector<vector<ll>> ans(R1,vector<ll>(C2,0));
+
+    for(ll i=0;i<R1;i++) {
+        for(ll j=0;j<C2;j++) { 
+            for(ll k=0;k<R2;k++) {
+                (ans[i][j] += (a[i][k]%mod * b[k][j]%mod)%mod) %= mod;
+            }
+        } 
+    }
+
+    return ans;
+}
+
 vector<vector<ll>> MatrixExpo(vector<vector<ll>>& a,ll n){
 
     vector<vector<ll>> result(a.size(),vector<ll>(a.size(),0));
